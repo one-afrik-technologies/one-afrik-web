e7580b54098b7264583445ea254bb8623ad0d04b
Mutshinye <Mutshinyen@gmail.com>
2024-07-11 13:31:37 +0200

    index file which includes all necessary pages using the include-once function

---
+++ frontend/index.php
@@ -0,0 +1,13 @@
+<?php
+// Include the header file
+include_once 'header.php';
+
+// Include the navigation bar
+include_once 'navbar.php';
+
+// Include the main content
+include_once 'content.php';
+
+// Include the footer file
+include_once 'footer.php';
+?>
