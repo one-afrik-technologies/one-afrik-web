2b9686d76ae6e12a9ee7d481d2d0199deae83dec
mohanned-sameh <mohanned2678@gmail.com>
2024-07-31 20:21:27 +0300

    feat: Add admin-only auth and route protection
    - Added `is_admin` to users table
    - Created admin middleware and `isAdmin` method
    - Secured admin routes for projects and services

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\AuthController;
 
 // Home page
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -23,3 +24,28 @@ Route::get('/projects/{id}', [ProjectController::class, 'show'])->name('projects
 // Services routes
 Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
 Route::get('/services/{id}', [ServiceController::class, 'show'])->name('services.show');
+
+// Auth routes
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Admin routes
+Route::get('/admin', [HomeController::class, 'admin'])->name('admin')->middleware('admin');
+Route::get('/admin/contacts', [ContactController::class, 'admin'])->name('admin.contacts')->middleware('admin');
+
+// admin projects routes
+Route::get('/admin/projects', [ProjectController::class, 'admin'])->name('admin.projects')->middleware('admin');
+Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create')->middleware('admin');
+Route::post('/admin/projects', [ProjectController::class, 'store'])->name('admin.projects.store')->middleware('admin');
+Route::get('/admin/projects/{id}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit')->middleware('admin');
+Route::put('/admin/projects/{id}', [ProjectController::class, 'update'])->name('admin.projects.update')->middleware('admin');
+Route::delete('/admin/projects/{id}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy')->middleware('admin');
+
+// admin services routes
+Route::get('/admin/services', [ServiceController::class, 'admin'])->name('admin.services')->middleware('admin');
+Route::get('/admin/services/create', [ServiceController::class, 'create'])->name('admin.services.create')->middleware('admin');
+Route::post('/admin/services', [ServiceController::class, 'store'])->name('admin.services.store')->middleware('admin');
+Route::get('/admin/services/{id}/edit', [ServiceController::class, 'edit'])->name('admin.services.edit')->middleware('admin');
+Route::put('/admin/services/{id}', [ServiceController::class, 'update'])->name('admin.services.update')->middleware('admin');
+Route::delete('/admin/services/{id}', [ServiceController::class, 'destroy'])->name('admin.services.destroy')->middleware('admin');
