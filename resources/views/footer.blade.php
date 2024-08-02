b5490ee53605c871af34b32d13ccc646558fcde6
jallow's <bjallow000@outlook.com>
2024-08-02 13:15:26 +0000

    completed the footer section for large screen

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,17 +4,56 @@
 
 
 </div>
-<ul class="socials">
-    <li>
+    
+    <div class="socials">
+        
+    <div>
+        <p>Follow us on</p>
+        <ul class="media">
+
+         <li>
         <a href="#"><i class="ri-facebook-circle-fill"></i></a>
-    </li>
-    <li>
-        <a href="#"><i class="ri-twitter-fill"></i></a>
-    </li>
-    <li>
-        <a href="#"><i class="ri-youtube-fill"></i></a>
-    </li>
-</ul>
+        </li>
+        <li>
+            <a href="#"><i class="ri-twitter-fill"></i></a>
+        </li>
+        <li>
+            <a href="#"><i class="ri-youtube-fill"></i></a>
+        </li>
+        </ul>
+    </div>
+   
+<div class="socials2">
+    <div class="resources3">
+        <ul>
+             <li><a href="#">Services</a></li>
+        <li><a href="#">resources</a></li>
+        </ul>
+       
+    </div>
+ <div class="resource2">
+    <ul>
+        
+        <li><a href="#">privacy and policy</a></li>
+        <li><a href="#">faqs</a></li>
+        <li><a href="#">affliates</a></li>
+    </ul>
+ </div>
+ <div class="resource1">
+    <ul>
+        <li><a href="#">about us</a></li>
+         <li><a href="#">contact us</a></li>
+         <li><a href="#">support</a></li>
+          
+   </ul>
+  </div>
+  
+</div>
+   
+</div>
+
+
+<hr>
 <div class="header__bar">
     Copyright &copy; <?php echo date('Y'); ?>  One Afrik Technologies 
 </div>
