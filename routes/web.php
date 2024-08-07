8b4012bc8a5bf17f25036c29efb01dbbe7c8d89f
mohanned-sameh <mohanned2678@gmail.com>
2024-08-07 05:33:47 +0300

    Refactor ContactController and ProjectController to add admin panel functionality

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::view('/about', 'about')->name('about');
 // Contacts routes
 Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+
 
 // Projects routes
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
@@ -34,14 +37,8 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 // Admin routes
 Route::get('/admin', [DashboardController::class, 'index'])->name('admin')->middleware('admin');
 Route::get('/admin/contacts', [ContactController::class, 'admin'])->name('admin.contacts')->middleware('admin');
-
-// admin projects routes
-Route::get('/admin/projects', [ProjectController::class, 'admin'])->name('admin.projects')->middleware('admin');
-Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create')->middleware('admin');
-Route::post('/admin/projects', [ProjectController::class, 'store'])->name('admin.projects.store')->middleware('admin');
-Route::get('/admin/projects/{id}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit')->middleware('admin');
-Route::put('/admin/projects/{id}', [ProjectController::class, 'update'])->name('admin.projects.update')->middleware('admin');
-Route::delete('/admin/projects/{id}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy')->middleware('admin');
+Route::get('/admin/contacts/{id}', [ContactController::class, 'show'])->name('admin.contacts.show')->middleware('admin');
+Route::delete('/admin/contacts/{id}', [ContactController::class, 'destroy'])->name('admin.contacts.destroy')->middleware('admin');
 
 // admin services routes
 Route::get('/admin/services', [ServiceController::class, 'admin'])->name('admin.services')->middleware('admin');
@@ -50,3 +47,11 @@ Route::post('/admin/services', [ServiceController::class, 'store'])->name('admin
 Route::get('/admin/services/{id}/edit', [ServiceController::class, 'edit'])->name('admin.services.edit')->middleware('admin');
 Route::put('/admin/services/{id}', [ServiceController::class, 'update'])->name('admin.services.update')->middleware('admin');
 Route::delete('/admin/services/{id}', [ServiceController::class, 'destroy'])->name('admin.services.destroy')->middleware('admin');
+
+// admin projects routes
+Route::get('/admin/projects', [ProjectController::class, 'admin'])->name('admin.projects')->middleware('admin');
+Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create')->middleware('admin');
+Route::post('/admin/projects', [ProjectController::class, 'store'])->name('admin.projects.store')->middleware('admin');
+Route::get('/admin/projects/{id}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit')->middleware('admin');
+Route::put('/admin/projects/{id}', [ProjectController::class, 'update'])->name('admin.projects.update')->middleware('admin');
+Route::delete('/admin/projects/{id}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy')->middleware('admin');
