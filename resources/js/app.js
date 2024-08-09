ce3658e512ddfb0593b44862cac91afe7779f52f
jallow's <bjallow000@outlook.com>
2024-08-09 10:10:42 +0000

    rearranged the footer section

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open");
-
+ console.log('nav-clicked')
   const isOpen = navLinks.classList.contains("open");
   menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
